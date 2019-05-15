40013d99b8c197df6da9efa1c77c02748a9c4aaa
weishuyong1996 <svipwsy@163.com>
2019-05-15 10:16:33 +0800

    Android加载js方法、js文件html自带

--- app/src/main/java/com/example/filedownloader/MainActivity.java
+++ app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.filedownloader;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +15,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,6 +24,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -32,28 +38,33 @@ import okio.Okio;
 import okio.Sink;
 
 /*
- *
- *
  * Weview加载js、html文件
  *
  * */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private WebView webView;
 
+    //todo 如果是assert不是下载文件的话直接用
+    //webView.loadUrl("file:////android_asset/A4.html");
+    @SuppressLint("JavascriptInterface")
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
-        webView.getSettings().setAllowFileAccess(true);
         //支持放大网页功能
-        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setAllowFileAccess(true);
         //支持缩小网页功能
-        webView.getSettings().setBuiltInZoomControls(true);
-        //支持JAVA
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDefaultTextEncodingName("GBK");//设置字符编码
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.addJavascriptInterface(MainActivity.this, "java");//name:android在网页里面可以用window.name.方法名调用java方法
         findViewById(R.id.tv_download).setOnClickListener(this);
-
     }
 
     @Override
@@ -66,63 +77,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         downloadFile();
                     }
                 }).start();
-                webView.setWebViewClient(new WebViewClient() {
-                    @Override
-                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                        super.onPageStarted(view, url, favicon);
-
-                    }
-
-                    @Override
-                    public void onPageFinished(WebView view, String url) {
-                        super.onPageFinished(view, url);
-                        //getLocalStorageData(view);
-                        //todo webView加载js文件
-                        loadAssert("file:///mnt/sdcard/aa1.js", getApplicationContext());
-                    }
-                });
-
                 break;
         }
-    }
-
-
-    public String loadAssert(String path, Context context) {
-        if (context == null || TextUtils.isEmpty(path)) {
-            return null;
-        }
-
-        InputStream inputStream = null;
-        BufferedReader bufferedReader = null;
-        try {
-            File f = new File(path);
-            inputStream = new FileInputStream(f);
-            // inputStream = context.getAssets().open(path);
-            StringBuilder builder = new StringBuilder(inputStream.available() + 10);
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            char[] data = new char[4096];
-            int len = -1;
-            while ((len = bufferedReader.read(data)) > 0) {
-                builder.append(data, 0, len);
-            }
-
-            return builder.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (bufferedReader != null)
-                    bufferedReader.close();
-            } catch (IOException e) {
-            }
-            try {
-                if (inputStream != null)
-                    inputStream.close();
-            } catch (IOException e) {
-            }
-        }
-
-        return "";
     }
 
     private void downloadFile() {
@@ -142,12 +98,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onResponse(okhttp3.Call call, Response response) throws IOException {
-
                 Sink sink = null;
                 BufferedSink bufferedSink = null;
                 try {
                     String mSDCardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
-                    File dest = new File(mSDCardPath, "A4.html");
+                    File dest = new File(mSDCardPath, "web1.html");
                     sink = Okio.sink(dest);
                     bufferedSink = Okio.buffer(sink);
                     bufferedSink.writeAll(response.body().source());
@@ -158,7 +113,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     webView.post(new Runnable() {
                         @Override
                         public void run() {
-                            webView.loadUrl("file:///mnt/sdcard/A4.html");
+                            //todo 先加载网页html
+                            webView.loadUrl("file:///mnt/sdcard/a1.html");
+                            //todo 这是调用assert下的 下载无法到assert文件夹下，所以---
+//                            webView.loadUrl("file:////android_asset/A4.html");
+                            webView.setWebViewClient(new WebViewClient() {
+
+                                @Override
+                                public void onPageFinished(WebView view, String url) {
+                                    super.onPageFinished(view, url);
+                                    //调用Js方法 可用
+//                                    webView.loadUrl("javascript:javaToJS()");
+                                }
+                            });
+
                         }
                     });
                 } catch (Exception e) {
@@ -173,4 +141,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+
 }
